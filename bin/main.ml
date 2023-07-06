7c8d84315f765c2bbc0e0583d7121c3533d48514
Jay Lee <jaeho.lee@snu.ac.kr>
2023-07-06 16:29:00 +0900

    :sparkles: Collecting semantics

--- bin/main.ml
+++ bin/main.ml
@@ -3,6 +3,14 @@ open Stdio
 open Abstracting_definitional_interpreters.Definitional_interpreter
 
 let expr =
+  Op2
+    {
+      op = "*";
+      lhs = Op2 { op = "+"; lhs = Const 3; rhs = Const 4 };
+      rhs = Const 9;
+    }
+
+let _expr =
   App
     {
       operator =
@@ -10,11 +18,12 @@ let expr =
       operand = Op2 { op = "+"; lhs = Const 1; rhs = Const 2 };
     }
 
-let { result; store } = Default_interpreter.eval expr
+let { result; store; trace } = Collecting_interpreter.eval expr
 
 let () =
   printf "expr: %s\n" (show_expr expr);
   (match result with
   | Ok value -> printf "value: %s\n" (show_value value)
   | Error exn -> printf "exception: %s\n" (Exn.to_string exn));
-  printf "store: %s\n" (show_store store)
+  printf "store: %s\n" (show_store store);
+  printf "trace: %s\n" (show_trace trace)
--- lib/definitional_interpreter.ml
+++ lib/definitional_interpreter.ml
@@ -27,7 +27,7 @@ let show_env env =
   let show_pair (x, loc) = Printf.sprintf "%s : %d" x loc in
   let pairs = Map.to_alist env in
   let pairs_str = List.map pairs ~f:show_pair |> String.concat ~sep:", " in
-  Printf.sprintf "{ %s }" pairs_str
+  if String.is_empty pairs_str then "{}" else Printf.sprintf "{ %s }" pairs_str
 
 let show_value = function
   | Num n -> "Num" ^ Int.to_string n
@@ -38,7 +38,7 @@ let show_store store =
   let show_pair (loc, v) = Printf.sprintf "%d : %s" loc (show_value v) in
   let pairs = Map.to_alist store in
   let pairs_str = List.map pairs ~f:show_pair |> String.concat ~sep:", " in
-  Printf.sprintf "{ %s }" pairs_str
+  if String.is_empty pairs_str then "{}" else Printf.sprintf "{ %s }" pairs_str
 
 let rec fix f x = f (fix f) x
 
@@ -53,6 +53,7 @@ end
 module type Components = sig
   type 'a t
   type result
+  type eval = expr -> value t
 
   module M : Computation with type 'a t := 'a t
 
@@ -65,7 +66,7 @@ module type Components = sig
   val find_env : environment -> string -> loc
   val augment : environment -> string -> loc -> environment
   val local_env : environment -> value t -> value t
-  val mrun : value t -> result
+  val eval : (eval -> eval) -> expr -> result
 end
 
 module type Interpreter = sig
@@ -77,7 +78,7 @@ end
 module Make_interpreter (C : Components) :
   Interpreter with type result := C.result = struct
   type result = C.result
-  type eval = expr -> value C.t
+  type eval = C.eval
 
   let ev (ev : eval) : eval =
     let open C in
@@ -118,7 +119,7 @@ module Make_interpreter (C : Components) :
             local_env env' (ev body)
         | _ -> fail (Type_error { op = "application"; value = closure }))
 
-  let eval (e : expr) : result = C.mrun ((fix ev) e)
+  let eval (e : expr) : result = C.eval ev e
 end
 
 type default_result = { result : (value, exn) Result.t; store : store }
@@ -157,6 +158,7 @@ struct
   module M = Environment
 
   type 'a t = 'a M.t
+  type eval = expr -> value t
 
   let run_op2 (op : string) (lhs : value) (rhs : value) : value M.t =
     let m =
@@ -214,6 +216,150 @@ struct
   let mrun (m : value M.t) : result =
     let result, store = Store.(run ExnState.(run M.(run m empty)) empty) in
     { result; store }
+
+  let eval (ev : eval -> eval) (e : expr) : result = mrun ((fix ev) e)
 end
 
 module Default_interpreter = Make_interpreter (Default_components)
+
+type trace_elem = expr * environment * store
+type trace = trace_elem list
+
+let show_trace (trace : trace) : string =
+  let show_tuple (e, env, store) =
+    Printf.sprintf "<%s, %s, %s>" (show_expr e) (show_env env)
+      (show_store store)
+  in
+  let tuples_str =
+    trace |> List.map ~f:show_tuple |> String.concat ~sep:";\n"
+  in
+  if String.is_empty tuples_str then "[]"
+  else Printf.sprintf "[ %s ]" tuples_str
+
+type collecting_result = {
+  result : (value, exn) Result.t;
+  store : store;
+  trace : trace;
+}
+
+module Collecting_components : Components with type result = collecting_result =
+struct
+  type result = collecting_result
+
+  module Trace = struct
+    module T = struct
+      type t = trace
+
+      include Monoid.List.Make (struct
+        type t = trace_elem
+      end)
+    end
+
+    include Monad.Writer.T1 (T) (Monad.Ident)
+    include Monad.Writer.Make (T) (Monad.Ident)
+  end
+
+  module Store = struct
+    module T = struct
+      type t = store
+    end
+
+    include Monad.State.T1 (T) (Trace)
+    include Monad.State.Make (T) (Trace)
+
+    let empty = Map.empty (module Int)
+  end
+
+  module ExnState = struct
+    include Monad.Result.Exception.T (Store)
+    include Monad.Result.Exception.Make (Store)
+  end
+
+  module Environment = struct
+    module T = struct
+      type t = environment
+    end
+
+    include Monad.Reader.T1 (T) (ExnState)
+    include Monad.Reader.Make (T) (ExnState)
+
+    let empty = Map.empty (module String)
+  end
+
+  module M = Environment
+
+  type 'a t = 'a M.t
+  type eval = expr -> value t
+
+  let run_op2 (op : string) (lhs : value) (rhs : value) : value M.t =
+    let m =
+      let open ExnState in
+      match (op, lhs, rhs) with
+      | "+", Num lhs, Num rhs -> return (Num (lhs + rhs))
+      | "-", Num lhs, Num rhs -> return (Num (lhs - rhs))
+      | "*", Num lhs, Num rhs -> return (Num (lhs * rhs))
+      | "/", Num lhs, Num rhs ->
+          if rhs = 0 then fail Division_by_zero else return (Num (lhs / rhs))
+      | op, Num _, Num _ -> fail (Unbound_operator op)
+      | op, Num _, _ -> fail (Type_error { op; value = rhs })
+      | op, _, _ -> fail (Type_error { op; value = lhs })
+    in
+    M.lift @@ m
+
+  let fail (e : exn) : value M.t = M.lift @@ ExnState.fail e
+
+  let find (loc : loc) : value M.t =
+    let m =
+      let open Store in
+      let* store = get () in
+      let value = Map.find_exn store loc in
+      return value
+    in
+    M.lift @@ ExnState.lift @@ m
+
+  let alloc (_x : string) : loc M.t =
+    let m =
+      let open Store in
+      let* store = get () in
+      let new_loc = Map.length store in
+      return new_loc
+    in
+    M.lift @@ ExnState.lift @@ m
+
+  let extend (loc : loc) (v : value) : unit M.t =
+    let m =
+      let open Store in
+      let* store = get () in
+      let store' = Map.set store ~key:loc ~data:v in
+      put store'
+    in
+    M.lift @@ ExnState.lift @@ m
+
+  let ask_env : unit -> environment M.t = M.read
+  let find_env (env : environment) (x : string) : loc = Map.find_exn env x
+
+  let augment (env : environment) (x : string) (loc : loc) : environment =
+    Map.set env ~key:x ~data:loc
+
+  let local_env (env : environment) (m : value M.t) : value M.t =
+    M.(lift (run m env))
+
+  let ev_tell (ev : eval -> eval) (ev' : eval) (e : expr) : value M.t =
+    let open M in
+    let* env = ask_env () in
+    let* store = lift @@ ExnState.lift @@ Store.get () in
+    let* () =
+      lift @@ ExnState.lift @@ Store.lift @@ Trace.write [ (e, env, store) ]
+    in
+    (ev ev') e
+
+  let mrun (m : value M.t) : result =
+    let (result, store), trace =
+      Trace.(run Store.(run ExnState.(run M.(run m empty)) empty))
+    in
+    { result; store; trace }
+
+  let eval (ev : eval -> eval) (e : expr) : result = mrun ((fix (ev_tell ev)) e)
+end
+
+module Collecting_interpreter = Make_interpreter (Collecting_components)
